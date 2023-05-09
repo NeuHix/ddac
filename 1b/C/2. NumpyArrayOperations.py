4fd3854694392f8e24a3da7b827ad392754c8bda
Sachin Saurav <radonarc@duck.com>
2023-05-09 15:23:40 +0530

    made mean_datasets function

--- 1b/C/2. NumpyArrayOperations.py
+++ 1b/C/2. NumpyArrayOperations.py
@@ -1,5 +1,40 @@
 import numpy as np
 
-listArray = np.array([[2, 4.5, 3.6], [3, 4.2, 3.1]])
+"""
+2D Array Slicing in Numpy
+"""
 
-print(listArray)
+# 1D Array in Numpy
+OneDArrayInNp = np.array([1, 3.8, 1.8, 0.8])
+print(OneDArrayInNp[::2])
+
+# 2D Array in Numpy
+TwoDArrayInNp = np.array([[2, 4.5, 3.6], [3, 4.2, 3.1]])
+
+print(TwoDArrayInNp, "\n")
+print(TwoDArrayInNp[0:2, 1])
+
+'''
+1D Array Slicing just works like ordinary list slicing 
+because, well, it just takes a single list of floats to make one.
+
+2D Array Slicing works little differently.
+
+whole list = [[2, 4.5, 3.6], [3, 4.2, 3.1]]
+first daughter list = [2, 4.5, 3.6]
+second daughter list = [3, 4.2, 3.1]
+
+It is a list of two lists which contain floats as their elements.
+		2dArray[x,y]
+		x = which element of the list you want to access ?
+			the first element of the "whole list" is the 
+			first "daughter list"
+		
+		y = then you do normal list slicing on the specified 
+			"daughter list" in "x"
+		
+If you wanna do list slicing on both "daughter list" , 
+you specify "0:2" or ":" in "x"
+
+learned this here: https://www.w3schools.com/python/numpy/numpy_array_slicing.asp
+'''
---
+++ 1b/C/3. MeanOfASetOfSignals.py
@@ -0,0 +1,57 @@
+'''
+
+>> > mean_datasets(['data1.csv', 'data2.csv', 'data3.csv'])
+array([[ 11.   11.9  13. ]
+       [  9.5   6.8   9.4]
+       [  7.2  11.1  12.5]
+       [  8.8   7.3   9.2]
+       [ 16.6  10.6  10.3]])
+
+'''
+import numpy as np
+
+
+def mean_datasets(filesAsInput):
+    currentListIndex = 0
+    indexInsideList = 0
+
+    ArrayFiles = []
+    RowList = []
+    calc_meanList = []
+    meanList = []
+    threeCalcMeans = []
+
+    for fileToArray in filesAsInput:
+        filesMadeArray = np.loadtxt(fileToArray, delimiter=',')
+        ArrayFiles.append(filesMadeArray)
+
+    print(ArrayFiles)
+
+    dimensionOfArrayWeGot = np.shape(ArrayFiles[0])
+
+    for RowLength in range(0, dimensionOfArrayWeGot[0]):
+        for columnLength in range(0, dimensionOfArrayWeGot[1]):
+            for IterateArray in ArrayFiles:
+                RowList.append(IterateArray[currentListIndex][indexInsideList])
+            calc_meanList.append(RowList)
+            RowList = []
+            if indexInsideList < dimensionOfArrayWeGot[1] - 1:
+                indexInsideList += 1
+        if currentListIndex < dimensionOfArrayWeGot[0]:
+            currentListIndex += 1
+            indexInsideList = 0
+
+    for CalcMean in calc_meanList:
+        resultMean = np.round(np.mean(CalcMean), decimals=1)
+        threeCalcMeans.append(resultMean)
+        if len(threeCalcMeans) == dimensionOfArrayWeGot[1]:
+            meanList.append(threeCalcMeans)
+            threeCalcMeans = []
+
+    ArrayedMeans = np.array(meanList)
+
+    return ArrayedMeans
+
+if __name__ == '__main__':
+  test = mean_datasets(['data1.csv', 'data2.csv', 'data3.csv'])
+  print(test.shape)
